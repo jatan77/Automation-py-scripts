9374e53c15a93e38640d21618ab5974e90e250d7
ninjaj$&$N <jatanvora111@gmail.com>
2020-05-05 13:48:11 +0530

    upload file01
    play script to play with URLs and save titles

---
+++ py-play.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+for i in range(34000, 60000):
+    url="https://kmhd.link/archives/"+str(i)+""
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "lxml")
+    print(soup.title)
+    if soup.title.text == "Page not found – KMHD Links":
+        print("skipped--"+url+"\n")
+    else:
+        print("Saved--"+url+"\n")
+        file = open('movie-links.txt','a') 
+        file.write("\n----------------------------------------------------\n")
+        file.write(url+"----"+soup.title.text) 
+        file.close() 
