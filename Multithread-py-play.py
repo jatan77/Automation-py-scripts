2c52742bf5529fd5fc69d376199e56e556214db8
ninjaj$&$N <jatanvora111@gmail.com>
2020-05-05 15:21:23 +0530

    Update Multithread-py-play.py

--- Multithread-py-play.py
+++ Multithread-py-play.py
@@ -43,6 +43,10 @@ for i in range(concurrent):
     t.daemon = True
     t.start()
 try:
+    '''
+    for url in open('urllist.txt'):
+        q.put(url.strip())            #to take urls from txt file
+    '''
     for i in range(50000, 60000):
         url="https://example.com/id/file/"+str(i)+""
         q.put(url)
