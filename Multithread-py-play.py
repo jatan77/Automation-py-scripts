8fb93c95523394d4820a53aa6f84ac0c1cc71be4
ninjaj$&$N <jatanvora111@gmail.com>
2020-05-05 14:56:58 +0530

    py-ply with mulithread to boos work speed

---
+++ Multithread-py-play.py
@@ -0,0 +1,50 @@
+from urllib.parse import urlparse
+from threading import Thread
+import http.client, sys
+from queue import Queue
+import requests
+from bs4 import BeautifulSoup
+
+concurrent = 10
+
+def doWork():
+    while True:
+        url = q.get()
+        print(url)
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, "lxml")
+        print((soup.title.text))
+        if soup.title.text == "Page not found – KMHD Links":
+            print("skipped--"+url+"\n")
+        else:
+            print("Saved--"+url+"\n")
+            file = open('movie-links.txt','a') 
+            file.write("\n----------------------------------------------------\n")
+            file.write(url+"----"+soup.title.text) 
+            file.close() 
+        q.task_done()
+
+def getStatus(ourl):
+    try:
+        url = urlparse(ourl)
+        conn = http.client.HTTPConnection(url.netloc)   
+        conn.request("HEAD", url.path)
+        res = conn.getresponse()
+        return res.status, ourl
+    except:
+        return "error", ourl
+
+def doSomethingWithResult(status, url):
+    print((status, url))
+
+q = Queue(concurrent * 2)
+for i in range(concurrent):
+    t = Thread(target=doWork)
+    t.daemon = True
+    t.start()
+try:
+    for url in open('urllist.txt'):
+        q.put(url.strip())
+    q.join()
+except KeyboardInterrupt:
+    sys.exit(1)
